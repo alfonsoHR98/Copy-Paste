d13ce755d090fe464442e2f7a43a35eb86895baa
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-06-10 17:25:20 -0600

    chore: Refactor file search and copying logic in busqueda.py

--- busqueda.py
+++ busqueda.py
@@ -1,19 +1,11 @@
 import os
 import shutil
-import tkinter as tk
-from tkinter import filedialog
 
-# Crear la ventana de la aplicación
-root = tk.Tk()
-root.withdraw()
+# ruta de la carpeta a buscar
+search_path = ""
 
-# Solicitar al usuario que seleccione la carpeta de búsqueda
-print("Seleccione la carpeta de búsqueda")
-search_path = filedialog.askdirectory()
-
-# Solicitar al usuario que seleccione la carpeta de destino
-print("Seleccione la carpeta de destino")
-save_path = filedialog.askdirectory()
+# ruta de la carpeta donde se guardarán los archivos encontrados
+save_path = ""
 
 #Terminaciones de los archivos a buscar
 file_terminals = ['PD.pdf', 'PS.pdf']
@@ -34,5 +26,4 @@ for file_name in file_names:
     matching_files = list(filter(lambda x: file_name in x and terminal in x, all_files))
     if len(matching_files) > 0:
       for file in matching_files:
-        shutil.copy(search_path + "/" + file, save_path)
-        print("Archivo encontrado: ", file)
+        shutil.copy(os.path.join(search_path, file), save_path)
